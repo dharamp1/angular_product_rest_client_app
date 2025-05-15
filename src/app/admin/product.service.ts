4c210860b110893f88f9370a09509fa232d99fcc
Dharmaraj Pawale <dharmaraj.pawale@gmail.com>
2025-05-15 14:02:10 +0530

    created product class and added getAllProducts() method in product service

--- src/app/admin/product.service.ts
+++ src/app/admin/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  PRODUCT_REST_API_BASE_URL = "http://localhost:8080/api/products"
+
+  constructor(private http:HttpClient) { }
+
+  getAllProducts(){
+    return this.http.get(this.PRODUCT_REST_API_BASE_URL)
+  }
 }
