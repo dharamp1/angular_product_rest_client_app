701c4f740d2b1c7578631a81966f74b448d3863c
Dharmaraj Pawale <dharmaraj.pawale@gmail.com>
2025-05-15 14:13:30 +0530

    Implemented Life Cycle Hook Interface OnInit and it's method ngOnInit(). Also injected product service

--- src/app/admin/product-view/product-view.component.ts
+++ src/app/admin/product-view/product-view.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.css']
 })
-export class ProductViewComponent {
+export class ProductViewComponent implements OnInit {
 
   products!:Product[];
+
+  constructor(private productService:ProductService){
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
