06d698fd1bc708e6f643be1df9699a81256c17bb
Dharmaraj Pawale <dharmaraj.pawale@gmail.com>
2025-05-15 15:31:42 +0530

    created product add form component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductAddFormComponent } from './product-add-form/product-add-form.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductViewComponent
+    ProductViewComponent,
+    ProductAddFormComponent
   ],
   imports: [
     CommonModule
